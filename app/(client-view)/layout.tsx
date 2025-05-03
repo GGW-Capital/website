39d5ae13a8fc48266705d820552807c9975b1d34
Hadi-Abdallah <hadiabdallah090@gmail.com>
2025-05-03 14:33:29 +0300

    feat: Integrate Google Analytics for enhanced tracking

--- app/(client-view)/layout.tsx
+++ app/(client-view)/layout.tsx
@@ -107,7 +107,16 @@ export default function RootLayout({
       lang="en"
       className={`${montserrat.variable} ${baskerville.variable} js`}
     >
-      <head>
+      <head> {/* Google Analytics */}
+        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-3YXKDW657X" strategy="afterInteractive" />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-3YXKDW657X');
+          `}
+        </Script>
         <Script
           dangerouslySetInnerHTML={{
             __html: `document.documentElement.classList.add('js')`,
